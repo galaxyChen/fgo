4b2c46c90404381985a726f547060f527da2214a
galaxy <549278303@qq.com>
2020-01-23 01:20:13 +0800

    修复抽池子的bug

--- 抽池子.py
+++ 抽池子.py
@@ -7,7 +7,7 @@ reset = readAndConvert('./img/重置.png')#800,540
 close = readAndConvert('./img/重置关闭.png') # 600,540
 full = readAndConvert("./img/抽满.png") #800,550
 handler = baseOperator()
-
+        
 
 def waitUntil(condition,x,y,handler,sleep=1):
     while True:
@@ -15,9 +15,9 @@ def waitUntil(condition,x,y,handler,sleep=1):
         if handler.checkCondition(img,condition,x,y,condition.size):
             time.sleep(sleep)
             return
-        handler.tap(1100,300)
+        handler.tap(430,450)
         time.sleep(sleep)
-        
+
 count = 0
 print("开始抽")
 while True:
@@ -34,11 +34,10 @@ while True:
         waitUntil(close, 600,540, handler, 0.5)
         print("关闭窗口")
         handler.tap(650,560)
-        handler.tap(650,560)
         print("开始抽")
+        continue
     # 抽满礼物箱
     if handler.checkCondition(img,full,800,550,full.size):
-        handler.tap(420,560)
         handler.tap(420,560)
         print("抽满，结束")
         break
